9e4713aa257cf2f52cd3be1fc001dc4acc43f9d1
Martin95056 <email_martin@abv.bg>
2016-03-14 17:22:49 +0200

    class Deck

--- ProBelot/card.py
+++ ProBelot/card.py
@@ -1,5 +1,6 @@
 from settings import trump, not_trump,\
                       CARD_TYPES, VALUES
+import random
 
 
 class Card:
@@ -8,6 +9,9 @@ class Card:
             self.value = value
         if card_type in CARD_TYPES:
             self.type = card_type
+
+    def __str__(self):
+        return "{} of {}".format(self.value, self.card_type)
 
     def get_rate(self, game_str):
         if game_str == 'All Trumps':
@@ -19,3 +23,8 @@ class Card:
                 return trump[self.value]
             else:
                 return not_trump[self.value]
+
+
+class Deck:
+    def __init__(self):
+        self.deck = random.shuffle([Card(v, t) for v in VALUES for t in CARD_TYPES])
