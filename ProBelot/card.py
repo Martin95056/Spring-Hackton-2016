f775ca12cc3e18f1e08bc946596e2fc90f1cc72b
Martin95056 <email_martin@abv.bg>
2016-03-14 16:56:14 +0200

    type to card_type

--- ProBelot/card.py
+++ ProBelot/card.py
@@ -3,11 +3,11 @@ from settings import trump, not_trump,\
 
 
 class Card:
-    def __init__(self, value, type):
+    def __init__(self, value, card_type):
         if value in VALUES:
             self.value = value
-        if type in CARD_TYPES:
-            self.type = type
+        if card_type in CARD_TYPES:
+            self.type = card_type
 
     def get_rate(self, game_str):
         if game_str == 'All Trumps':
