fade3079f50f88e73e0a3e84cf73c33507d09621
Martin95056 <email_martin@abv.bg>
2016-03-16 17:13:20 +0200

    valid_values is ready

--- ProBelot/game_logic.py
+++ ProBelot/game_logic.py
@@ -1,6 +1,5 @@
 from settings import CARD_TYPES
-from single_round import pregame
-from player import Player, ALL_GIVEN_CARDS, ALL_GIVEN_CARDS_IN_HAND
+import player as p
 
 
 def J_9_more(player):
@@ -21,24 +20,27 @@ def A_10_more(player):
                     return True
 
 
-def valid_values(player):
+def valid_values(player, game):
     same_type_cards = []
     different_type_cards = []
     for card in player.cards:
-        if card.card_type == ALL_GIVEN_CARDS_IN_HAND[0].card_type:
+        if card.type == p.ALL_GIVEN_CARDS_ON_TABLE[0].type:
             same_type_cards.append(card)
         else:
             different_type_cards.append(card)
 
-    valid_cards_of_same_type = []
+    if game == 'No Trumps':
+        if len(same_type_cards) > 0:
+            return same_type_cards
+        else:
+            return different_type_cards
 
-    for card in same_type_cards:
-        if card.get_index_by_value(pregame()) > ALL_GIVEN_CARDS_IN_HAND[len(ALL_GIVEN_CARDS_IN_HAND) - 1].get_index_by_value(pregame()):
-            valid_cards_of_same_type.append(card)
+    result = [c for c in same_type_cards if
+              c.get_rate(game) > p.ALL_GIVEN_CARDS_ON_TABLE[-1].get_rate(game)]
 
-    if valid_cards_of_same_type >= 1:
-        return valid_cards_of_same_type
-    elif same_type_cards >= 1:
+    if len(result) >= 1:
+        return result
+    elif len(same_type_cards) >= 1:
         return same_type_cards
     else:
         return different_type_cards
@@ -46,14 +48,14 @@ def valid_values(player):
 
 def all_trumps_logic(player, coplayer):
     # Когато играчът е пръв
-    if len(ALL_GIVEN_CARDS_IN_HAND) == 0:
+    if len(p.ALL_GIVEN_CARDS_IN_HAND) == 0:
         for c in player.cards:
-            #Играе 'А', ако имаш 'А' и '10'(и повече) от една боя
+            # Играе 'А', ако имаш 'А' и '10'(и повече) от една боя
             if J_9_more(player.cards):
                 pos = player.get_index_by_value('J')
                 player.throw_card(player.cards[pos])
-            
-            #Играе боята, която съотборникът му е казал
+
+            # Играе боята, която съотборникът му е казал
             elif player.has_cards_of_coplayer_game():
                 for v in player.get_all_values_of_one_type(coplayer.game_i_want):
                     pos = player.get_index_by_value(v)
@@ -62,13 +64,13 @@ def all_trumps_logic(player, coplayer):
 
             elif c.value == '9':
 
-                #Играе 9-ката ако Валето е минало
-                for x in ALL_GIVEN_CARDS:
+                # Играе 9-ката ако Валето е минало
+                for x in p.ALL_GIVEN_CARDS:
                     if x.value == 'J' and x.type == c.type:
                         player.throw_card(c)
 
                 vals = player.get_all_values_of_one_type(c.type)
-                #Играе '10' или 'А', ако има двойна 9ка
+                # Играе '10' или 'А', ако има двойна 9ка
                 if len(vals) == 2:
                     if '10' in vals:
                         pos = player.get_index_by_value('10')
@@ -79,7 +81,7 @@ def all_trumps_logic(player, coplayer):
                     else:
                         continue
 
-                #Играе някоя от картите от боята на 9-ката с цел избиване на вале
+                # Играе някоя от картите от боята на 9-ката с цел избиване на вале
                 elif len(vals) >= 3:
                     for v in vals:
                         if v != '9':
@@ -88,14 +90,14 @@ def all_trumps_logic(player, coplayer):
                             break
 
             else:
-                #Играе белот, ако не влезе в един от горните случаи
+                # Играе белот, ако не влезе в един от горните случаи
                 if player.has_belote():
                     pos = player.get_index_by_value('Q')
                     player.throw_card(player.cards[pos])
-                #Играе '7', ако няма нищо
+                # Играе '7', ако няма нищо
                 elif c.value == '7':
                     player.throw_card(c)
-                #Играе '8', ако няма нищо
+                # Играе '8', ако няма нищо
                 elif c.value == '8':
                     player.throw_card(c)
 
@@ -109,32 +111,33 @@ def all_trumps_logic(player, coplayer):
 
 
 def no_trumps_logic(player, coplayer):
-    if len(ALL_GIVEN_CARDS_IN_HAND) == 0:
+    if len(p.ALL_GIVEN_CARDS_IN_HAND) == 0:
         for c in player.cards:
-            #Играе 'А', ако имаш 'А' и '10'(и повече) от една боя
+            # Играе 'А', ако имаш 'А' и '10'(и повече) от една боя
             if A_10_more(player.cards):
                 pos = player.get_index_by_value('A')
                 player.throw_card(player.cards[pos])
 
-            #Игра 10-ката ако Асака е минал
+            # Игра 10-ката ако Асака е минал
             elif c.value == '10':
-                for x in ALL_GIVEN_CARDS:
+                for x in p.ALL_GIVEN_CARDS:
                     if x.value == 'A' and x.type == c.type:
                         player.throw_card(c)
 
                 vals = player.get_all_values_of_one_type(c.type)
-                #Проверка дали 10-ката е тройна
+                # Проверка дали 10-ката е тройна
                 if len(vals) >= 3:
-                    #Играе 'К'
+                    # Играе 'К'
                     if 'K' in vals:
                         pos = player.get_index_by_value('K')
                         player.throw_card(player.card[pos])
-                    #Играе някоя от картите от боята на 10-ката с цел избиване на асак
+                    # Играе някоя от картите от боята на 10-ката с цел избиване на асак
                     else:
                         for v in vals:
-                            if v.value != '10'
-                            pos = player.get_index_by_value(v)
-                            player.throw_card(player.card[pos])
+                            if v.value != '10':
+                                pos = player.get_index_by_value(v)
+                                player.throw_card(player.card[pos])
+
 
 def game_type_logic(game, player, coplayer):
     pass
